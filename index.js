49e80f682964a0c0b6c6b8ea58048d9fdfb2ecba
Farwa Sundas <farwasunda7@gmail.com>
2025-11-14 10:17:09 +0500

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const pwd = document.getElementById('pwd');
-    const toggle = document.getElementById('toggle');
-    toggle.addEventListener('change', () => {
-      pwd.type = toggle.checked ? 'text' : 'password';
-    });
